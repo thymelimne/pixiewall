79cb959a778ea8fd840bb018dded13686a409784
thymelimne <91765107+thymelimne@users.noreply.github.com>
2023-03-16 12:09:55 -0700

    Testing branches 5

--- silhouettesmode.py
+++ silhouettesmode.py
@@ -8,55 +8,73 @@ from random import random as rand
 import time
 
 kernel = np.ones((4, 8), np.float32) / 25
+th1 = 127
+th2 = 255
 ke1 = np.ones((6, 6), np.float32)
 kd1 = np.ones((6, 6), np.float32)
-starttime = 50 
+starttime = 0
 def capframe(vid, y1, y2, x1, x2, sub, screen, g):
     global kernel, ke1, kd1
 
-    # Frame
+    # Frame                     ~ video (but it's blue)
     ret, frame = vid.read()
 
-    # Crop
+    # Adjust colors             ~ correctly colored video
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    #'''
+    # Crop                      ~ cropped video
     frame = frame[y1:y2, x1:x2]
 
-    # Blur
-    #kernel = np.ones((4, 8), np.float32) / 25
+    #'''
+    # Blur                      ~ blurred video
     frame = cv2.filter2D(frame, -1, kernel)
 
-    # Threshold
-    ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    #'''
+    # Threshold                 ~ fiery orange effect video
+    ret, frame = cv2.threshold(frame, th1, th2, cv2.THRESH_BINARY)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
 
-    # Subtract background
+    #'''
+    # Subtract background, KNN  ~ powdery white over black
     frame = sub.apply(frame)
 
-    # Blur
+    #'''
+    # Blur                      ~ fuzzy, Rainbow Fish-esque silhouette
     frame = cv2.filter2D(frame, -1, kernel)
 
+    #'''
     # Threshold
-    ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    #ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
 
+    #'''
     # Erode
-    #ke1 = np.ones((6, 6), np.float32)
     frame = cv2.erode(frame, ke1, iterations=2)
 
+    #'''
     # Dilate
-    #kd1 = np.ones((6, 6), np.float32)
     frame = cv2.dilate(frame, kd1, iterations=2)
 
     # Avoid playing that blip of white-screen at the beginning
     if g.t < 5:
         frame *= 0
 
+    #'''
     #Put to the screen
     surface = pygame.surfarray.make_surface(np.rot90(frame))
     screen.blit(surface, (0, 0))
 
+    #'''
     #Switch the mode, if necessary.
     if cv2.countNonZero(frame) < 10 and g.t > 50:
         g.mode = 2
         return g.mode
     g.t += 1
+
+    #'''
 
 if __name__ == "__main__":
     time.sleep(2)
@@ -111,6 +129,8 @@ if __name__ == "__main__":
         currenttime = time.time() - starttime
         if currenttime > 100:
             running = False
+
+        g.t += 1
 
     # After the loop release the cap object
     vid.release()
