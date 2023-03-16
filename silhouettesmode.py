f7cd778f2d92aeaf5308c3710d510ef4dc4645fd
thymelimne <91765107+thymelimne@users.noreply.github.com>
2023-03-16 14:28:39 -0700

    Good fadein via threshold starting high then going down

--- silhouettesmode.py
+++ silhouettesmode.py
@@ -8,13 +8,13 @@ from random import random as rand
 import time
 
 kernel = np.ones((4, 8), np.float32) / 25
-th1 = 127
-th2 = 255
+threshold = 127
+th = 255
 ke1 = np.ones((6, 6), np.float32)
 kd1 = np.ones((6, 6), np.float32)
 starttime = 0
 def capframe(vid, y1, y2, x1, x2, sub, screen, g):
-    global kernel, ke1, kd1
+    global kernel, ke1, kd1, threshold, th
 
     # Frame                     ~ video (but it's blue)
     ret, frame = vid.read()
@@ -26,55 +26,47 @@ def capframe(vid, y1, y2, x1, x2, sub, screen, g):
     # Crop                      ~ cropped video
     frame = frame[y1:y2, x1:x2]
 
-    #'''
     # Blur                      ~ blurred video
     frame = cv2.filter2D(frame, -1, kernel)
 
-    #'''
     # Threshold                 ~ fiery orange effect video
-    ret, frame = cv2.threshold(frame, th1, th2, cv2.THRESH_BINARY)
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+    ret, frame = cv2.threshold(frame, th, 255, cv2.THRESH_BINARY)
 
-    #'''
     # Subtract background, KNN  ~ powdery white over black
     frame = sub.apply(frame)
 
-    #'''
     # Blur                      ~ fuzzy, Rainbow Fish-esque silhouette
     frame = cv2.filter2D(frame, -1, kernel)
 
-    #'''
     # Threshold
-    #ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+    ret, frame = cv2.threshold(frame, th, 255, cv2.THRESH_BINARY)
 
-    #'''
     # Erode
     frame = cv2.erode(frame, ke1, iterations=2)
 
-    #'''
     # Dilate
     frame = cv2.dilate(frame, kd1, iterations=2)
 
+    #'''
     # Avoid playing that blip of white-screen at the beginning
     if g.t < 5:
         frame *= 0
-
     #'''
+
+    if th > threshold:
+        th -= 1
+
     #Put to the screen
     surface = pygame.surfarray.make_surface(np.rot90(frame))
     screen.blit(surface, (0, 0))
 
-    #'''
+    '''
     #Switch the mode, if necessary.
     if cv2.countNonZero(frame) < 10 and g.t > 50:
         g.mode = 2
         return g.mode
     g.t += 1
-
-    #'''
+    '''
 
 if __name__ == "__main__":
     time.sleep(2)
